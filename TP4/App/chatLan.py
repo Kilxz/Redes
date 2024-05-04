35385ad9a988ef23dd7d803e85c7e55cdc9384b9
Kilxz <tomasrando@gmail.com>
2024-05-03 23:49:43 -0300

    Añadir nueva versión
    Se agrega la capacidad de hablar al mismo tiempo que se escucha.

--- TP4/App/chatLan.py
+++ TP4/App/chatLan.py
@@ -5,22 +5,24 @@ def speak():
     global stop
     socketSpeak = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     direction = ('192.168.1.255', 60000)
-    print("Ingrese el mensaje a enviar: ")
-    message = input()
-    if message == "salir":
-        stop = True
-    socketSpeak.sendto(message.encode(), direction)
-    socketSpeak.close()
+    while stop != True:
+        message = input()
+        socketSpeak.sendto(message.encode(), direction)
+        if message == "salir":
+            stop = True
+            socketSpeak.close()
 
 def listen():
+    global stop
     socketListen = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     socketListen.bind(('0.0.0.0', 60000))
-    received = socketListen.recvfrom(128)
-    message = received[0].decode()
-    if message == "salir":
-        print("El otro usuario ha salido")
-    else:
-        print("Mensaje recibido: ", message)
+    while stop != True:
+        received = socketListen.recvfrom(128)
+        message2 = received[0].decode()
+        if message2 == "salir":
+            print("El otro usuario ha salido")
+        else:
+            print("Mensaje recibido: ", message2)
 
 def initialize():
     global stop
